784165497c15765dfcb033257965e9e54e2e5dda
Igor Renan <igorenansc@gmail.com>
2023-03-30 07:18:26 -0300

    feat: users and matches migration created

---
+++ app/backend/src/database/migrations/20230329224803-Matches.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference:{
+          model:'teams',
+          key:'id'
+        }
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      away_team_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference:{
+          model:'teams',
+          key:'id'
+        }
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     */
+    await queryInterface.dropTable('matches');
+  }
+};
